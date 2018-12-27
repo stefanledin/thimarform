ef95ba67f690ba948ee1137fe7708488f5f0f606
Stefan Ledin <stefan.ledin@gmail.com>
2018-12-27 23:12:39 +0100

    Nyhetskollage

--- wp-content/themes/thimarform/header.php
+++ wp-content/themes/thimarform/header.php
@@ -10,7 +10,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<?php wp_head(); ?>
 	</head>
-	<body <?php body_class('p-4-md');?>>
+	<body <?php body_class('p-md-4');?>>
 
 		<nav class="mobile-menu">
 			<div class="mobile-menu__close">
