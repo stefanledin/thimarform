14543e8bd3c8f9e82812c0ef8f3c15c5ee903370
Stefan Ledin <stefan.ledin@gmail.com>
2019-04-08 18:23:25 +0200

    padding på sidorna

--- wp-content/themes/thimarform/sections/50_50_text_besides_image.php
+++ wp-content/themes/thimarform/sections/50_50_text_besides_image.php
@@ -1,5 +1,5 @@
 <?php if ( have_rows('columns') ) : ?>
-    <section data-aos="fade-up" class="mb-3 text-besides-image">
+    <section data-aos="fade-up" class="mb-3 text-besides-image px-sm-3">
         <div class="row no-gutters">
             <?php while (have_rows('columns')) : the_row(); ?>
                 <div class="col-sm-6 d-flex <?php echo ( get_row_index() == 1 ) ? 'order-sm-last' : 'order-sm-first';?>">
