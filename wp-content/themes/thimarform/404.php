ad29bc6b2397624e8badbbc5751f4a72aba57d16
Stefan Ledin <stefan.ledin@gmail.com>
2019-04-10 08:40:22 +0200

    404-ändringar

--- wp-content/themes/thimarform/404.php
+++ wp-content/themes/thimarform/404.php
@@ -10,9 +10,9 @@
         <div class="row no-gutters">
             <div class="col-10 offset-1 col-lg-8 offset-lg-2" style="min-height: 50vh;">
                 <div class="text-center py-5">
-                    <h1 class="h1--deluxe">404</h1>
+                    <h1 class="h1--deluxe">Här är det tomt!</h1>
                     <?php
-                    echo wpautop( get_field('404_message', 'option') );
+                    the_field('404_message', 'option');
                     ?>
                 </div>
             </div>
